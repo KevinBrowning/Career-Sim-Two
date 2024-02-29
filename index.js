33c0b8ccd8406ebf89d6c7cc68b25aa3e9be297a
Kevin Browning <kbrowning1996@gmail.com>
2024-02-29 16:46:17 -0500

    reformatted to 2 spaces tab size.

---
+++ index.js
@@ -0,0 +1,8 @@
+/* const urlParams = new URLSearchParams(window.location.search);
+const firstname = urlParams.get('name');
+
+if (firstname) {
+    document.getElementById("successh1").textContent = `Thanks ${firstname}!`;
+} else {
+    document.getElementById("successh1").textContent = "Thank you for joining!";
+} */
